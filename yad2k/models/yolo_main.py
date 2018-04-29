8fe745fbb12c776663965b930ecad681de6e4511
Mar <m_sefiani@hotmail.com>
2018-04-29 17:42:11 +0400

    Add files via upload
    Initial version to demonstrate functionality

---
+++ yad2k/models/yolo_main.py
@@ -0,0 +1,47 @@
+from keras import backend as K
+from keras.models import load_model
+from yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes
+from yad2k.models.keras_yolo import yolo_head
+from app_utils import yolo_eval
+from os import listdir
+from os.path import isfile, join
+import os
+
+# Create a session to start graph
+sess = K.get_session()
+
+class_names = read_classes("model_data/coco_classes.txt")
+anchors = read_anchors("model_data/yolo_anchors.txt")
+image_shape = (480., 640.)
+
+# Read yolo model
+yolo_model = load_model("model_data/yolo.h5")
+
+# Preprocessing on yolo_model.output
+yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+
+# Filter boxes on NMS and thresholding
+scores, boxes, classes = yolo_eval(yolo_outputs, image_shape)
+
+def predict(sess, image_file):
+ 
+    # Preprocess your image
+    image, image_data = preprocess_image("images/" + image_file, model_image_size=(608, 608))
+
+    # Run the yolo model
+    out_scores, out_boxes, out_classes = sess.run([scores, boxes, classes], feed_dict={yolo_model.input: image_data,
+                                                                                       K.learning_phase(): 0})
+
+    # Generate colors for drawing bounding boxes.
+    colors = generate_colors(class_names)
+    # Draw bounding boxes on the image file
+    draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
+    # Save the predicted bounding box on the image
+    image.save(os.path.join("out", image_file), quality=90)
+
+    return out_scores, out_boxes, out_classes
+
+# Run predict for all files in images/ directory and save it to out/ directory
+all_outputs = [predict(sess,f) for f in listdir('images/') if isfile(join('images/', f))]
+
+print('All outputs are saved in out/ directory')
